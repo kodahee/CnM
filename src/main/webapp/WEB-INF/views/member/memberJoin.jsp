2fe14e66ac43e6f29e9b8a476a4a8d70822243fb
kodahee <daheeko0604@naver.com>
2021-04-15 01:16:37 +0900

    join

--- src/main/webapp/WEB-INF/views/member/memberJoin.jsp
+++ src/main/webapp/WEB-INF/views/member/memberJoin.jsp
@@ -20,7 +20,7 @@
 				<label for="id">ID</label> 
 				<input type="text" class="form-control"	id="id" name="id">
 				<button type="button" class="btn btn-secondary" id="idCheck">중복검사</button>
-				<h6 id="idResult" name="idCheck"></h6>
+				<h6 id="idResult"></h6>
 				<!-- ID 는 6글자 이상 -->
 			</div>
 			<div class="form-group">
--- src/main/webapp/resources/jquery/memberJoin.js
+++ src/main/webapp/resources/jquery/memberJoin.js
@@ -2,15 +2,22 @@
  * 
  */
 
+let idResult = true;
+let idCheckResult = true;
+let pwResult = true;
+let pwCheckResult = true;
+
 // id 글자수 확인
 $("#id").blur(function() {
 	let id = $("#id").val().length;
 	console.log(id);
 	
-	if(id < 7) {
+	if(id < 6) {
 		$("#idResult").text("아이디는 6자리 이상이어야 합니다.");
+		idResult = false;
 	} else {
 		$("#idResult").text("");
+		idResult = true;
 	}
 });
 
@@ -25,10 +32,12 @@ $("#pw").blur(function() {
 	let pw = $("#pw").val().length;
 	console.log(pw);
 	
-	if(pw < 9) {
+	if(pw < 8) {
 		$("#pwResult").text("비밀번호는 8자리 이상이어야 합니다.");
+		pwResult = false;
 	} else {
 		$("#pwResult").text("");
+		idResult = true;
 	}
 });
 
@@ -36,8 +45,10 @@ $("#pw").blur(function() {
 $("#pwCheck").blur(function() {
 	if($("#pw").val() == $("#pwCheck").val()){
 		$("#pwCheckResult").text("비밀번호가 일치합니다.");
+		idResult = true;
 	} else {
 		$("#pwCheckResult").text("비밀번호가 일치하지 않습니다.");
+		pwCheckResult = false;
 	}
 });
 
@@ -45,8 +56,10 @@ $("#pwCheck").blur(function() {
 
 //--------------------------------------------------------------
 
+let etcResult = true;
+
 $("#btn").click(function(){
-	for(let e of etc){
+	for(let e of $(".etc")){
 		if(e.value == ""){
 			etcResult=false;
 			break;
@@ -54,9 +67,7 @@ $("#btn").click(function(){
 	}
 	
 	//조건이 만족하면
-	if(idCheckResult && pwCheckResult && pwEqualResult && etcResult){
-		let frm = document.getElementById("frm");
-		//frm.submit();
+	if(idResult && idCheckResult && pwResult && pwCheckResult && etcResult){
 		alert("회원가입 진행");
 	}else {
 		alert("필수 항목을 입력하세요");
