0f0c70a21b66d375d5e0f6853c60f0aec261ebbb
kodahee <daheeko0604@naver.com>
2021-04-16 17:32:31 +0900

    join profilePic

--- src/main/java/com/ed/cnm/member/MemberService.java
+++ src/main/java/com/ed/cnm/member/MemberService.java
@@ -1,21 +1,28 @@
 package com.ed.cnm.member;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.ed.cnm.util.FileManager;
 
 @Service
 public class MemberService {
 	
 	@Autowired
 	private MemberDAO memberDAO;
+	@Autowired
+	private FileManager fileManager;
 
 	// 회원정보수정
-	public int memberUpdate(MemberDTO memberDTO) throws Exception{
+	public int memberUpdate(MemberDTO memberDTO) throws Exception {
 		return memberDAO.memberUpdate(memberDTO);
 	}
 	
 	// 회원탈퇴
-	public int memberDelete(MemberDTO memberDTO) throws Exception{
+	public int memberDelete(MemberDTO memberDTO) throws Exception {
 		return memberDAO.memberDelete(memberDTO);
 	}
 	
@@ -24,14 +31,19 @@ public class MemberService {
 		return memberDAO.checkID(memberDTO);
 	}
 	
-	// 회원가입
-	public MemberDTO memberLogin(MemberDTO memberDTO) throws Exception{
+	// 로그인
+	public MemberDTO memberLogin(MemberDTO memberDTO) throws Exception {
 		return memberDAO.memberLogin(memberDTO);
 	}
 	
-	// 로그인
-	public int memberJoin(MemberDTO memberDTO) throws Exception{
+	// 회원가입
+	public int memberJoin(MemberDTO memberDTO, MultipartFile memberPic, HttpSession session) throws Exception {
+		String profilePic = fileManager.save("member", memberPic, session);
+		
+		memberDTO.setProfilePic(profilePic);
+		
 		int result = memberDAO.memberJoin(memberDTO);
+		
 		return result;
 	}
 	
