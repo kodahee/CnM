c51a3ba22800ab745a76a253caaf0827542a3c29
kodahee <daheeko0604@naver.com>
2021-04-19 15:37:00 +0900

    community DTO

--- src/main/java/com/ed/cnm/board/BoardFileDTO.java
+++ src/main/java/com/ed/cnm/board/BoardFileDTO.java
@@ -6,6 +6,29 @@ public class BoardFileDTO {
 	private String fileName;
 	private String originName;
 	
-	
+	public long getFileNum() {
+		return fileNum;
+	}
+	public void setFileNum(long fileNum) {
+		this.fileNum = fileNum;
+	}
+	public long getNum() {
+		return num;
+	}
+	public void setNum(long num) {
+		this.num = num;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public String getOriginName() {
+		return originName;
+	}
+	public void setOriginName(String originName) {
+		this.originName = originName;
+	}
 	
 }
