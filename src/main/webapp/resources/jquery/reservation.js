82141953f953af8aeeba1da95219186c11d2f35c
ba-lim <dmsqltkfk276@naver.com>
2021-04-19 15:20:02 +0900

    0419

--- src/main/webapp/resources/jquery/reservation.js
+++ src/main/webapp/resources/jquery/reservation.js
@@ -3,7 +3,6 @@
  */
 //전역 변수 
 let date = new Date();
-	
 //날짜 지정 메서드(하루 전을 파라미터로 넘겨야 함)
 function getDate(date){
 	let year = date.getFullYear();
@@ -26,25 +25,64 @@ function getDate(date){
 //영화목록 API에서 영화코드 응답받겠다.
 let movieNmList = [];
 let movieCdList=[];
+
 $('.movieNm').each(function(index, li){
 	//console.log(this);//엘리먼트 전부 뜸(태그까지)
 	//console.log($(this).text());//제목만 뜸
+	//console.log(index+' : '+movieNmList[index]);
 	movieNmList.push($(this).text());
-	console.log(index+' : '+movieNmList[index]);
-$.ajax({
-	url : "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json",
-	type:"GET",
-	data: {
-		key:"88c812405e947836cbbee3be8daa5603",
-		movieNm : movieNmList[index]
-	},
-	success: function(data) {
-		console.log(index+' : '+movieNmList[index]);
-		//console.log(data);
-		console.log('===');
-	}
-		
-		
-});
-
+	
+	$.ajax({
+		url : "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json",
+		type:"GET",
+		data: {
+			key:"88c812405e947836cbbee3be8daa5603",
+			movieNm : movieNmList[index]
+		},
+		success: function(data) {
+			//console.log(index+' : '+movieNmList[index]);
+			data = data.movieListResult.movieList//데이터가 여러개 올 가능성 있음
+			//1. 이름이 완전히 같아야 하고, 2. movieCd가 제일 큰수 일때 배열로 추가하겟다.
+			let equalNmToCd=[];
+			for(movie of data){//데이터(배열)을 반복하면서 
+				if(movieNmList[index]==movie.movieNm){//이름이 같은걸 찾고 같다면
+					equalNmToCd.push(movie.movieCd);//영화코드를 배열에 담는다.
+				}
+			}
+			//이름이 같은 것들 중에서 제일 큰수를 뽑겠다.
+			let movieCd=Math.max.apply(null,equalNmToCd);
+			//console.log('이름'+index+': ' +movie.movieNm+' 코드: '+movieCd)//성공!
+			movieCdList.push(movieCd);
+			
+			//저 코드를 가지고 다시 함수를 호출해야함,,
+		}//success end
+	});//ajax end
 });//each end 29번줄
+			console.log(movieCdList.length);
+
+for(index in movieCdList){
+	
+	$.ajax({
+		url:"http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json",
+		type:"GET",
+		data:{
+			key:"88c812405e947836cbbee3be8daa5603",
+			movieCd:movieCdList[index]
+		},
+		success:function(result){
+			alert('hi')
+			result=result.movieInfoResult.movieInfo
+			console.log(result)
+			console.log(result.openDt)
+			console.log(result.audits.watchGradeNm)
+			console.log(movieCdList[index]+'=='+result.movieCd)
+			console.log(result.movieNm)
+			
+			//append를 사용할거임,,
+			
+		}
+		
+	});
+	
+	
+};//movieCdList for end
