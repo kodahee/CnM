2fc769f6171d3976fdfb962a79e48f8393ff7392
kodahee <daheeko0604@naver.com>
2021-04-21 22:03:33 +0900

    notice

---
+++ src/main/java/com/ed/cnm/board/notice/NoticeDAO.java
@@ -0,0 +1,80 @@
+package com.ed.cnm.board.notice;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.ed.cnm.board.BoardDAO;
+import com.ed.cnm.board.BoardDTO;
+import com.ed.cnm.board.BoardFileDTO;
+import com.ed.cnm.util.Pager;
+
+@Repository
+public class NoticeDAO implements BoardDAO {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	private String NAMESPACE = "com.ed.cnm.board.notice.NoticeDAO.";
+	
+	@Override
+	public List<BoardDTO> getList(Pager pager) throws Exception {
+		// TODO Auto-generated method stub
+		return sqlSession.selectList(NAMESPACE+"getList", pager);
+	}
+	
+	@Override
+	public long getTotalCount(Pager pager) throws Exception {
+		// TODO Auto-generated method stub
+		return sqlSession.selectOne(NAMESPACE+"getTotalCount", pager);
+	}
+	
+	@Override
+	public BoardDTO getSelect(BoardDTO boardDTO) throws Exception {
+		// TODO Auto-generated method stub
+		return sqlSession.selectOne(NAMESPACE+"getSelect", boardDTO);
+	}
+	
+	public int setInsert(BoardDTO boardDTO) throws Exception {
+		return sqlSession.insert(NAMESPACE+"setInsert", boardDTO);
+	}
+	
+	public long getNum() throws Exception {
+		return sqlSession.selectOne(NAMESPACE+"getNum");
+	}
+	
+	public int setUpdate(BoardDTO boardDTO) throws Exception {
+		return sqlSession.update(NAMESPACE+"setUpdate", boardDTO);
+	}
+	
+	@Override
+	public int setDelete(BoardDTO boardDTO) throws Exception {
+		// TODO Auto-generated method stub
+		return sqlSession.delete(NAMESPACE+"setDelete", boardDTO);
+	}
+	
+	public BoardFileDTO getFileSelect(BoardFileDTO boardFileDTO) throws Exception {
+		return sqlSession.selectOne(NAMESPACE+"getFileSelect", boardFileDTO);
+	}
+	
+	@Override
+	public int setFileInsert(BoardFileDTO boardFileDTO) throws Exception {
+		// TODO Auto-generated method stub
+		return sqlSession.insert(NAMESPACE+"setFileInsert", boardFileDTO);
+	}
+	
+	@Override
+	public int setFileDelete(BoardFileDTO boardFileDTO) throws Exception {
+		// TODO Auto-generated method stub
+		return sqlSession.delete(NAMESPACE+"setFileDelete", boardFileDTO);
+	}
+	
+	@Override
+	public int setHitUpdate(BoardDTO boardDTO) throws Exception {
+		// TODO Auto-generated method stub
+		return sqlSession.update(NAMESPACE+"setHitUpdate", boardDTO);
+	}
+
+}
